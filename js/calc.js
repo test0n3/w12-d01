3ef166233b95a17446cfb35e6ec9a945600f0b5f
Carlos A <cayala.w@gmail.com>
2019-05-13 13:44:28 -0500

    Add JS code

---
+++ js/calc.js
@@ -0,0 +1,41 @@
+
+
+const sum = () => {
+  const $first = document.getElementById("first").value;
+  const $second = document.getElementById("second").value;
+  result(parseInt($first) + parseInt($second));
+}
+
+const minus = () => {
+  const $first = document.getElementById("first").value;
+  const $second = document.getElementById("second").value;
+  result(parseInt($first) - parseInt($second));
+}
+
+const multi = () => {
+  const $first = document.getElementById("first").value;
+  const $second = document.getElementById("second").value;
+  result(parseInt($first) * parseInt($second));
+}
+
+const divi = () => {
+  const $first = document.getElementById("first").value;
+  const $second = document.getElementById("second").value;
+  result(parseInt($first) / parseInt($second));
+}
+
+const power = () => {
+  const $first = document.getElementById("first").value;
+  const $second = document.getElementById("second").value;
+  result(Math.pow(parseInt($first), parseInt($second)));
+}
+
+const squareroot = () => {
+  const $first = document.getElementById("first").value;
+  const $second = document.getElementById("second").value;
+  result(Math.pow(parseInt($first), 1/parseInt($second)));
+}
+
+const result = (myResult) => {
+  document.getElementById("results").innerHTML = myResult;
+}
